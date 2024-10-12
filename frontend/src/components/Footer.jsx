cf530d9376e7fcfdc7624d540afb76c510547ab0
Divyansh Sharma <142254266+divi2806@users.noreply.github.com>
2024-10-12 13:54:00 +0530

    Initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2024 Crowdfunding Platform. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
